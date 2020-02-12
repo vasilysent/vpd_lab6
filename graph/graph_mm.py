c4a7eeec775ceb70c56358fb5ea787d3755e1791
Elementarno <edwardpro9@gmail.com>
2020-02-12 12:14:31 +0300

    Update graphs and model

--- graph/graph_mm.py
+++ graph/graph_mm.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 files = [
-  ['data/model.txt', 'Траектория (модель)']
+  ['data/m1.txt', 'Траектория (модель)']
   #['data/data10.txt', 'Движение в (1, 0)'],
   #['data/data-10.txt', 'Движение в (-1, 0)'],
   #['data/data0-1.txt', 'Движение в (0, -1)'],
@@ -17,24 +17,20 @@ for file in files:
 
   with open(file[0], 'r') as f:
     for line in f:
-      line = line.split(',')
-      if len(line) == 2:
-        x.append(float(line[0]))
-        y.append(float(line[1]))
-      elif len(line) == 4:
-        time.append(float(line[0]))
-        x.append(float(line[1]))
-        y.append(float(line[2]))
-        angle.append(float(line[3]))
+      line = line.split(' ')
+      x.append(float(line[0]) / 1000) # mm -> m
+      y.append(float(line[1]) / 1000) # mm -> m
 
   data.append([time, x, y, angle, file[1]])
 
 for graph in data:
   plt.plot(graph[1], graph[2], label=graph[4])
 
+for dot in [[0.35, 0], [0.35, 0.35], [0, 0.35], [0, 0]]:
+  plt.scatter(dot[0], dot[1], marker='.', s=250, color="orange")
 
 plt.title(r'Движения робота')
-plt.axis([-1, 1, -1, 1])
+plt.axis([-0.25, 0.75, -0.25, 0.75])
 plt.xlabel(r'x, м')
 plt.ylabel(r'y, м')
 
--- model/func.sce
+++ model/func.sce
@@ -4,3 +4,13 @@ function y=countup(a,b)
     end
     y = b
 endfunction
+
+function y=chooseone(mat,i,j)
+    if i > size(mat)(1) then
+        i = size(mat)(1)
+    end
+    if i < 1 then
+        i = 1
+    end
+    y=mat(i,j)
+endfunction
