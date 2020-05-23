94f9b1465f61e1f337940922d74da783687a3799
Elementarno <edwardpro9@gmail.com>
2020-05-23 17:14:21 +0300

    Add pics of almost all graphs
    Need to correct model datafile (model_data/lin-0.5_0.txt)

--- graph/graph_mm.py
+++ graph/graph_mm.py
@@ -1,25 +1,37 @@
 import matplotlib.pyplot as plt
 
 files = [
-  #['data/linear_exp.txt', 'Траектория (эксп.), линейное управление'],
-  #['data/hyperbolic_dich.txt', 'Траектория (эксп.), нелинейное управление'],
-  #['data/nonlinear_exp.txt', 'Траектория (эксп.), нелинейное управление'],
+  #['data/linear_exp.txt', 'Траектория (эксп.), линейное управление', 1],
+  #['data/hyperbolic_dich.txt', 'Траектория (эксп.), нелинейное управление', 1],
+  #['data/nonlinear_exp.txt', 'Траектория (эксп.), нелинейное управление', 1],
   
-  #['data/linear_exp_0_0.5.txt', 'Движение в (0, 500), лин.'],
-  #['data/linear_exp_0.5_0.txt', 'Движение в (500, 0), лин.'],
-  #['data/linear_exp_0_-0.5.txt', 'Движение в (0, -500), лин.'],
-  #['data/linear_exp_-0.5_0.txt', 'Движение в (-500, 0), лин.'],
-  #['data/nonlinear_exp_0_0.5.txt', 'Движение в (0, 500), нелин.'],
-  #['data/nonlinear_exp_0.5_0.txt', 'Движение в (500, 0), нелин.'],
-  #['data/nonlinear_exp_0_-0.5.txt', 'Движение в (0, -500), нелин.'],
-  #['data/nonlinear_exp_-0.5_0.txt', 'Движение в (-500, 0), нелин.'],
-  #['data/data10.txt', 'Движение в (1, 0)'],
-  #['data/data-10.txt', 'Движение в (-1, 0)'],
-  #['data/data0-1.txt', 'Движение в (0, -1)'],
-  ['data/model_nonlinear.txt', 'Траектория (модель.), нелинейное управление'],
-  #['data/linear_exp_(k_p=4, k_a=15).txt', 'Траектория (эксп.), линейное управление'],
+  #['data/linear_exp_0_0.5.txt', 'Движение в (0, 500), лин. (эксп.)', 1],
+  #['data/linear_exp_0.5_0.txt', 'Движение в (500, 0), лин. (эксп.)', 1],
+  #['data/linear_exp_0_-0.5.txt', 'Движение в (0, -500), лин. (эксп.)', 1],
+  #['data/linear_exp_-0.5_0.txt', 'Движение в (-500, 0), лин. (эксп.)', 1],
+  #['data/nonlinear_exp_0_0.5.txt', 'Движение в (0, 500), нелин. (эксп.)', 1],
+  #['data/nonlinear_exp_0.5_0.txt', 'Движение в (500, 0), нелин. (эксп.)', 1],
+  #['data/nonlinear_exp_0_-0.5.txt', 'Движение в (0, -500), нелин. (эксп.)', 1],
+  #['data/nonlinear_exp_-0.5_0.txt', 'Движение в (-500, 0), нелин. (эксп.)', 1],
+  
+  #['model_data/lin0_0.5.txt', 'Движение в (0, 500), лин. (модель)', 1000],
+  #['model_data/lin0.5_0.txt', 'Движение в (500, 0), лин. (модель)', 1000],
+  #['model_data/lin0_-0.5.txt', 'Движение в (0, -500), лин. (модель)', 1000],
+  #['model_data/lin-0.5_0.txt', 'Движение в (-500, 0), лин. (модель)', 1000],
+  #['model_data/nonlin0_500.txt', 'Движение в (0, 500), нелин. (модель)', 1],
+  #['model_data/nonlin500_0.txt', 'Движение в (500, 0), нелин. (модель)', 1],
+  #['model_data/nonlin0_-500.txt', 'Движение в (0, -500), нелин. (модель)', 1],
+  #['model_data/nonlin-500_0.txt', 'Движение в (-500, 0), нелин. (модель)', 1],
+
+  
+  ['data/linear_exp.txt', 'Движение по квадрату, лин. (эксп.)', 1],
+  ['data/nonlinear_exp.txt', 'Движение по квадрату, нелин. (эксп.)', 1],
+  
+  #['model_data/Lin.txt', 'Движение по квадрату, лин. (модель)', 1000],
+  #['model_data/Nonlin.txt', 'Движение по квадрату, нелин. (модель)', 1],
 ]
 
+factor = 1000
 data = []
 square_length = 500 # mm
 points = [[square_length, 0], [square_length, square_length], [0, square_length], [0, 0]]
@@ -35,8 +47,8 @@ for file in files:
   with open(file[0], 'r') as f:
     for line in f:
       line = line.split(' ')
-      x.append(float(line[0])) # mm -> m
-      y.append(float(line[1])) # mm -> m
+      x.append(float(line[0]) * file[2]) # mm -> m
+      y.append(float(line[1]) * file[2]) # mm -> m
 
   data.append([time, x, y, angle, file[1]])
 
@@ -47,8 +59,8 @@ for dot in points:
   plt.scatter(dot[0], dot[1], marker='.', s=250, color="orange")
 
 plt.title(r'Траектории движения робота')
-#plt.axis([-600, 600, -600, 600])
-plt.axis([-100, 600, -100, 600])
+#plt.axis([-640, 640, -640, 640])
+plt.axis([-140, 640, -140, 640])
 plt.xlabel(r'x, мм')
 plt.ylabel(r'y, мм')
 
@@ -58,5 +70,6 @@ plt.grid(which='minor', color='grey', linestyle=':')
 plt.minorticks_on()
 
 plt.legend(loc="lower left")
+#plt.legend(loc="upper left")
 
 plt.show()
